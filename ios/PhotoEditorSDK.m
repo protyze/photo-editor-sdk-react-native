a87fc7d762cde820466b1a66a5323482e98c6596
Jeroen <jeroen@onstuimig.nl>
2018-03-27 14:56:42 +0200

    Custom Filters, Stickers and Overlays for iOS Bugfixes

--- ios/PhotoEditorSDK.m
+++ ios/PhotoEditorSDK.m
@@ -17,6 +17,8 @@ NSString* const kBackgroundColorCameraKey = @"backgroundColorCamera";
 NSString* const kCameraRollAllowedKey = @"cameraRowAllowed";
 NSString* const kShowFiltersInCameraKey = @"showFiltersInCamera";
 
+BOOL *processedCustomItems = NO;
+
 // Menu items
 typedef enum {
     transformTool,
@@ -46,13 +48,13 @@ RCT_EXPORT_MODULE(PESDK);
 static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 +(NSString *) randomStringWithLength: (int) len {
-    
+
     NSMutableString *randomString = [NSMutableString stringWithCapacity: len];
-    
+
     for (int i=0; i<len; i++) {
         [randomString appendFormat: @"%C", [letters characterAtIndex: arc4random_uniform([letters length])]];
     }
-    
+
     return randomString;
 }
 
@@ -80,16 +82,16 @@ static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY
     };
 }
 
--(void)_openEditor: (UIImage *)image config: (PESDKConfiguration *)config features: (NSArray*)features resolve: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject {
+-(void)_openEditor: (UIImage *)image config: (PESDKConfiguration *)config features: (NSArray*)features custom:(NSDictionary*) custom resolve: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject {
     self.resolver = resolve;
     self.rejecter = reject;
-    
+
     // Just an empty model
     PESDKPhotoEditModel* photoEditModel = [[PESDKPhotoEditModel alloc] init];
-    
+
     // Build the menu items from the features array if present
     NSMutableArray<PESDKPhotoEditMenuItem *>* menuItems = [[NSMutableArray alloc] init];
-    
+
     // Default features
     if (features == nil || [features count] == 0) {
         features = @[
@@ -104,7 +106,7 @@ static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY
           [NSNumber numberWithInt: magic]
         ];
     }
-    
+
     [features enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         int feature = [obj intValue];
         switch (feature) {
@@ -166,13 +168,169 @@ static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY
                 break;
         }
     }];
-    
-    
+
+	/* Check for custom items */
+	if ( custom != nil  && processedCustomItems != YES )
+	{
+		NSNumber *includeDefaultFilters = [NSNumber numberWithBool:YES];
+		if ([custom valueForKey:@"includeDefaultFilters"]) {
+			includeDefaultFilters = [custom valueForKey:@"includeDefaultFilters"];
+		}
+		NSNumber *includeDefaultOverlays = [NSNumber numberWithBool:YES];
+		if ([custom valueForKey:@"includeDefaultOverlays"]) {
+			includeDefaultOverlays = [custom valueForKey:@"includeDefaultOverlays"];
+		}
+		NSNumber *includeDefaultStickerCategories = [NSNumber numberWithBool:YES];
+		if ([custom valueForKey:@"includeDefaultStickerCategories"]) {
+			includeDefaultStickerCategories = [custom valueForKey:@"includeDefaultStickerCategories"];
+		}
+
+		/* Set custom Filters */
+		if ([custom valueForKey:@"filters"] || [includeDefaultFilters boolValue] == NO)
+		{
+			/* Set Default Filter Array */
+			NSMutableArray<PESDKPhotoEffect *> *filters = [[NSMutableArray alloc] init];
+
+			if( [includeDefaultFilters boolValue] == YES ) {
+				filters = [[PESDKPhotoEffect allEffects] mutableCopy];
+			}else{
+				[filters addObject:[[PESDKPhotoEffect alloc] initWithIdentifier:@"None" lutURL:nil displayName: @"None"]];
+			}
+
+			/* Set Fitlers */
+			if ([custom valueForKey:@"filters"] )
+			{
+				NSArray<NSDictionary *> *filtersConfig = [custom valueForKey:@"filters"];
+				NSEnumerator *enumerator = [filtersConfig objectEnumerator];
+				id filter;
+				while (filter = [enumerator nextObject]) {
+					NSString *filter_id = [filter valueForKey:@"id"];
+					NSString *filter_label = [filter valueForKey:@"label"];
+
+					NSString *baseRes = [@"res/" stringByAppendingString:filter_id];
+
+					[filters addObject:[[PESDKPhotoEffect alloc] initWithIdentifier:filter_id lutURL:[[NSBundle mainBundle] URLForResource:baseRes withExtension:@"png"] displayName:filter_label]];
+				}
+
+				PESDKPhotoEffect.allEffects = [filters copy];
+			}
+		}
+
+		/* Set custom Overlays */
+		if ([custom valueForKey:@"overlays"] || [includeDefaultOverlays boolValue] == NO)
+		{
+			/* Set Default Overlay Array */
+			NSMutableArray<PESDKOverlay *> *overlays = [[NSMutableArray alloc] init];
+
+			if( [includeDefaultOverlays boolValue] == YES ) {
+				overlays = [[PESDKOverlay all] mutableCopy];
+			}else{
+				[overlays addObject:[PESDKOverlay none]];
+			}
+
+			/* Set Overlays */
+			if ([custom valueForKey:@"overlays"] )
+			{
+				NSArray<NSDictionary *> *overlaysConfig = [custom valueForKey:@"overlays"];
+				NSEnumerator *enumerator = [overlaysConfig objectEnumerator];
+				id overlay;
+				while (overlay = [enumerator nextObject]) {
+					NSString *overlay_id = [overlay valueForKey:@"id"];
+					NSString *overlay_label = [overlay valueForKey:@"label"];
+					NSString *overlay_blendmode = [[overlay valueForKey:@"blendMode"] lowercaseString];
+
+					int defaultBlendMode = PESDKBlendModeNormal;
+					if([overlay_blendmode isEqualToString:@"color_burn"]){
+                    	defaultBlendMode = PESDKBlendModeColorBurn;
+                    }else if([overlay_blendmode isEqualToString:@"darken"]){
+                        defaultBlendMode = PESDKBlendModeDarken;
+                    }else if([overlay_blendmode isEqualToString:@"lighten"]){
+                        defaultBlendMode = PESDKBlendModeLighten;
+                    }else if([overlay_blendmode isEqualToString:@"hard_light"]){
+                        defaultBlendMode = PESDKBlendModeHardLight;
+                    }else if([overlay_blendmode isEqualToString:@"soft_light"]){
+                        defaultBlendMode = PESDKBlendModeSoftLight;
+                    }else if([overlay_blendmode isEqualToString:@"multiply"]){
+                        defaultBlendMode = PESDKBlendModeMultiply;
+                    }else if([overlay_blendmode isEqualToString:@"overlay"]){
+                        defaultBlendMode = PESDKBlendModeOverlay;
+                    }else if([overlay_blendmode isEqualToString:@"screen"]){
+                        defaultBlendMode = PESDKBlendModeScreen;
+                    }else if([overlay_blendmode isEqualToString:@"normal"]){
+                        defaultBlendMode = PESDKBlendModeNormal;
+                    }
+
+					NSString *baseRes = [@"res/" stringByAppendingString:overlay_id];
+
+					[overlays addObject:[[PESDKOverlay alloc] initWithIdentifier:overlay_id displayName:overlay_label url:[[NSBundle mainBundle] URLForResource:baseRes withExtension:@"png"] thumbnailURL:[[NSBundle mainBundle] URLForResource:[baseRes stringByAppendingString:@"_thumb"] withExtension:@"png"] initialBlendMode:defaultBlendMode]];
+				}
+
+				PESDKOverlay.all = [overlays copy];
+			}
+		}
+
+
+
+		/* Set custom Stickers */
+		if ([custom valueForKey:@"stickerCategories"] || [includeDefaultStickerCategories boolValue] == NO)
+		{
+			/* Set Default Sticker Category Array */
+			NSMutableArray<PESDKStickerCategory *> *stickerCats = [[NSMutableArray alloc] init];
+
+			if( [includeDefaultStickerCategories boolValue] == YES ) {
+				stickerCats = [[PESDKStickerCategory all] mutableCopy];
+			}
+
+			/* Set Sticker Categories */
+			if ([custom valueForKey:@"stickerCategories"] )
+			{
+				NSArray<NSDictionary *> *stickerCatsConfig = [custom valueForKey:@"stickerCategories"];
+				NSEnumerator *enumerator = [stickerCatsConfig objectEnumerator];
+				id stickerCat;
+				while (stickerCat = [enumerator nextObject]) {
+					NSString *stickerCat_id = [stickerCat valueForKey:@"id"];
+					NSString *stickerCat_label = [stickerCat valueForKey:@"label"];
+
+					NSMutableArray<PESDKSticker *> *stickers = [[NSMutableArray alloc] init];
+
+					/* Set Stickers */
+					if ([stickerCat valueForKey:@"stickers"] )
+					{
+						NSArray<NSDictionary *> *stickersArray = [stickerCat valueForKey:@"stickers"];
+						NSEnumerator *stickerEnumerator = [stickersArray objectEnumerator];
+						id sticker;
+						while (sticker = [stickerEnumerator nextObject]) {
+							NSString *sticker_id = [sticker valueForKey:@"id"];
+							NSString *sticker_label = sticker_id;
+							if ([sticker valueForKey:@"label"] )
+								sticker_label = [sticker valueForKey:@"label"];
+
+							NSString *baseRes = [@"res/" stringByAppendingString:sticker_id];
+
+							[stickers addObject:[[PESDKSticker alloc] initWithImageURL:[[NSBundle mainBundle] URLForResource:baseRes withExtension:@"png"] thumbnailURL:[[NSBundle mainBundle] URLForResource:[baseRes stringByAppendingString:@"_thumb"] withExtension:@"png"] identifier:sticker_id]];
+						}
+					}
+
+					NSString *catBaseRes = [@"res/" stringByAppendingString:stickerCat_id];
+
+					[stickerCats addObject:[[PESDKStickerCategory alloc] initWithTitle:stickerCat_label imageURL:[[NSBundle mainBundle] URLForResource:catBaseRes withExtension:@"png"] stickers:stickers]];
+				}
+
+				PESDKStickerCategory.all = [stickerCats copy];
+			}
+
+
+		}
+
+		processedCustomItems = YES;
+	}
+
+
     self.editController = [[PESDKPhotoEditViewController alloc] initWithPhoto:image configuration:config menuItems:menuItems photoEditModel:photoEditModel];
-    
+
     self.editController.delegate = self;
     UIViewController *currentViewController = RCTPresentedViewController();
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
         [currentViewController presentViewController:self.editController animated:YES completion:nil];
     });
@@ -184,38 +342,38 @@ static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY
             if ([options valueForKey:kBackgroundColorEditorKey]) {
                 b.backgroundColor = [AVHexColor colorWithHexString: [options valueForKey:kBackgroundColorEditorKey]];
             }
-            
+
             if ([options valueForKey:kBackgroundColorMenuEditorKey]) {
                 b.menuBackgroundColor = [AVHexColor colorWithHexString: [options valueForKey:kBackgroundColorMenuEditorKey]];
             }
-            
+
         }];
-        
+
         [builder configureCameraViewController:^(PESDKCameraViewControllerOptionsBuilder * b) {
             if ([options valueForKey:kBackgroundColorCameraKey]) {
                 b.backgroundColor = [AVHexColor colorWithHexString: (NSString*)[options valueForKey:kBackgroundColorCameraKey]];
             }
-            
+
             if ([[options allKeys] containsObject:kCameraRollAllowedKey]) {
                 b.showCameraRoll = [[options valueForKey:kCameraRollAllowedKey] boolValue];
             }
-            
+
             if ([[options allKeys] containsObject: kShowFiltersInCameraKey]) {
                 b.showFilters = [[options valueForKey:kShowFiltersInCameraKey] boolValue];
             }
-            
+
             // TODO: Video recording not supported currently
             b.allowedRecordingModesAsNSNumbers = @[[NSNumber numberWithInteger:RecordingModePhoto]];
         }];
     }];
-    
+
     return config;
 }
 
-RCT_EXPORT_METHOD(openEditor: (NSString*)path options: (NSArray *)features options: (NSDictionary*) options resolve: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(openEditor: (NSString*)path options: (NSArray *)features options: (NSDictionary*) options custom:(NSDictionary*) custom resolve: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject) {
     UIImage* image = [UIImage imageWithContentsOfFile: path];
     PESDKConfiguration* config = [self _buildConfig:options];
-    [self _openEditor:image config:config features:features resolve:resolve reject:reject];
+    [self _openEditor:image config:config features:features custom:custom resolve:resolve reject:reject];
 }
 
 - (void)close {
@@ -223,25 +381,25 @@ RCT_EXPORT_METHOD(openEditor: (NSString*)path options: (NSArray *)features optio
     [currentViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
-RCT_EXPORT_METHOD(openCamera: (NSArray*) features options:(NSDictionary*) options resolve: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(openCamera: (NSArray*) features options:(NSDictionary*) options custom:(NSDictionary*) custom resolve: (RCTPromiseResolveBlock)resolve reject: (RCTPromiseRejectBlock)reject) {
     __weak typeof(self) weakSelf = self;
     UIViewController *currentViewController = RCTPresentedViewController();
     PESDKConfiguration* config = [self _buildConfig:options];
-    
+
     self.cameraController = [[PESDKCameraViewController alloc] initWithConfiguration:config];
 
     [self.cameraController.cameraController setupWithInitialRecordingMode:RecordingModePhoto error:nil];
-    
+
     UISwipeGestureRecognizer* swipeDownRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(close)];
     swipeDownRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
-    
+
     [self.cameraController.view addGestureRecognizer:swipeDownRecognizer];
     [self.cameraController setCompletionBlock:^(UIImage * image, NSURL * _) {
         [currentViewController dismissViewControllerAnimated:YES completion:^{
-            [weakSelf _openEditor:image config:config features:features resolve:resolve reject:reject];
+            [weakSelf _openEditor:image config:config features:features custom:custom resolve:resolve reject:reject];
         }];
     }];
-    
+
     [currentViewController presentViewController:self.cameraController animated:YES completion:nil];
 }
 
@@ -262,7 +420,7 @@ RCT_EXPORT_METHOD(openCamera: (NSArray*) features options:(NSDictionary*) option
         dispatch_async(dispatch_get_main_queue(), ^{
             [self.editController.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
         });
-        
+
     }
 }
 
@@ -273,13 +431,13 @@ RCT_EXPORT_METHOD(openCamera: (NSArray*) features options:(NSDictionary*) option
     NSString *randomPath = [PhotoEditorSDK randomStringWithLength:10];
     NSString* path = [documentsDirectory stringByAppendingPathComponent:
                       [randomPath stringByAppendingString:@".jpg"] ];
-    
+
     [data writeToFile:path atomically:YES];
     self.resolver(path);
     dispatch_async(dispatch_get_main_queue(), ^{
         [self.editController.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
     });
-    
+
 }
 
 @end
